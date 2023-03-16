87918b6f203785fdc1c7b699cfbc7939e2f7d3ad
Amol <amolk986@gmail.com>
2023-03-16 10:42:15 -0700

    structural directive ngfor and property binding

--- src/app/MyBazaar/item-list/item-list.component.ts
+++ src/app/MyBazaar/item-list/item-list.component.ts
@@ -7,6 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemListComponent implements OnInit {
 
+
+  items:Array<any> = [
+    {
+    "Id":1,
+    "itemName":"water bottel",
+    "price":200,
+    "MFG":"sura waters"
+  },
+  {
+    "Id":2,
+    "itemName":"water bottel",
+    "price":300,
+    "MFG":"sura waters"
+  },
+  {
+    "Id":3,
+    "itemName":"water bottel",
+    "price":400,
+    "MFG":"sura waters"
+  },
+  {
+    "Id":4,
+    "itemName":"water bottel",
+    "price":500,
+    "MFG":"sura waters"
+  },
+  {
+    "Id":5,
+    "itemName":"water bottel",
+    "price":600,
+    "MFG":"sura waters"
+  },
+  {
+    "Id":6,
+    "itemName":"water bottel",
+    "price":700,
+    "MFG":"sura waters"
+  },
+]
+
   constructor() { }
 
   ngOnInit(): void {
